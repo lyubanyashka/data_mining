09b38d23aca7fc0eb241e2595a1ae0c602c5be58
al-belyakov <al-belyakov@yandex-team.ru>
2019-12-28 09:29:42 +0300

    qqqq

--- tweet_analysis.py
+++ tweet_analysis.py
@@ -3,13 +3,10 @@ from nltk.corpus import stopwords
 import pymorphy2
 import nltk
 
-nltk.download('stopwords')
+#nltk.download()
+#nltk.download('stopwords')
 
 morph = pymorphy2.MorphAnalyzer()
-
-
-def primary_tweet_analysis(tweet: str):
-    return tweet
 
 
 def filter_chars(i):
@@ -26,8 +23,6 @@ positive_idx = 2
 
 class TwitterAnalyzer:
     def __init__(self):
-        self.total_words_count = 0
-
         self.stop_words = stopwords.words('russian')
         self.stop_words.extend(stopwords.words('english'))
         self.stop_words.extend(['россияхорватия', 'ruscro', 'worldcup',
@@ -44,8 +39,6 @@ class TwitterAnalyzer:
         self.word_to_estimation = dict()
 
     def get_estimation(self):
-        if type(self.word_to_estimation) is dict and len(self.word_to_estimation) != 0:
-            return self.word_to_estimation
         f = open(estimation_file_name, "r")
         lines = f.readlines()
         for line in lines:
@@ -58,7 +51,8 @@ class TwitterAnalyzer:
     def do(self):
         self.frequency_analysis()
         self.save_word_frequency()
-        #self.save_word_estimation()
+        self.save_tweets_length()
+        # self.save_word_estimation()
         self.general_tweet_estimate()
         self.top5_adj()
         rules_res = ""
@@ -113,11 +107,10 @@ class TwitterAnalyzer:
         self.get_estimation()
         for tweet in self.tweets:
             estimations_counter = [0, 0, 0]
-            words = tweet.split()
-            for word in words:
+            for word in tweet:
                 estimation = self.word_to_estimation.get(word, 0) + 1
                 estimations_counter[estimation] += 1
-            self.tweet_to_estimation[tweet] = estimations_counter
+            self.tweet_to_estimation[' '.join(tweet)] = estimations_counter
 
     def save_word_estimation(self):
         f = open(estimation_file_name, "w+")
@@ -131,7 +124,7 @@ class TwitterAnalyzer:
         for item in self.sorted_word_count_pair:
             word = item[0]
             count = item[1]
-            printed_str = "{} - {} - {:.3}%\n".format(word, count, 100 * count / self.total_words_count)
+            printed_str = "{} - {} - {:.3}%\n".format(word, count, 100 * count / len(self.tweets))
             f.write(printed_str)
         f.close()
 
@@ -145,7 +138,7 @@ class TwitterAnalyzer:
             f.write(printed_str)
         f.close()
 
-    def printAdj(self, f, top5_adj, header):
+    def printAdjective(self, f, top5_adj, header):
         f.write(header)
         for item in top5_adj:
             word = item[0]
@@ -174,37 +167,35 @@ class TwitterAnalyzer:
         top5_negative_adj = negative_adj[:5]
 
         f = open("adjectives.txt", "w+")
-        self.printAdj(f, top5_pos_adj, "Top-5 Positive:")
+        self.printAdjective(f, top5_pos_adj, "Top-5 Positive:\n")
         f.write("\n")
-        self.printAdj(f, top5_negative_adj, "Top-5 Negative:")
+        self.printAdjective(f, top5_negative_adj, "Top-5 Negative:\n")
         f.close()
 
     def frequency_analysis(self):
-        word_to_count = dict()
-        tweets_len_to_count = dict()
         text = self.data
-        if type(text) is not str:
-            return word_to_count, tweets_len_to_count
-
-        text = text.lower()
+        text = text.lower().replace("\n\n", "\n")
         text = ''.join([i for i in text if filter_chars(i)])
-        end_of_tweet = morph.parse(" |ENDOFTWEET| ")[0].normal_form
-        text = text.replace("\n\n", end_of_tweet)
-        words = nltk.word_tokenize(text, 'russian')
-        words = [word for word in words if not word.startswith('http')]
-        words = [word for word in words if word not in self.stop_words]
+        cache = dict()
+        self.tweets = text.splitlines()
+        for idx in range(len(self.tweets)):
+            self.tweets[idx] = nltk.word_tokenize(self.tweets[idx], 'russian')
+            self.tweets[idx] = [word for word in self.tweets[idx] if not word.startswith('http')]
+            self.tweets[idx] = [word for word in self.tweets[idx] if word not in self.stop_words]
+            normalized_words = []
+            for word in self.tweets[idx]:
+                if word not in cache:
+                    cache[word] = morph.parse(word)[0].normal_form
+                normalized_words.append(cache[word])
+            self.tweets[idx] = normalized_words
 
-        words = [morph.parse(word)[0].normal_form for word in words]
-
-        self.tweets = text.split(end_of_tweet)
+        tweets_len_to_count = dict()
         for tweet in self.tweets:
-            tweet_len = len(tweet.split())
+            tweet_len = len(tweet)
             tweets_len_to_count[tweet_len] = tweets_len_to_count.get(tweet_len, 0) + 1
-
-        words = [word for word in words if word not in end_of_tweet]
-        self.total_words_count = len(words)
-        for word in words:
-            self.word_to_count[word] = self.word_to_count.get(word, 0) + 1
+            tweet_words_set = set(tweet)
+            for word in tweet_words_set:
+                self.word_to_count[word] = self.word_to_count.get(word, 0) + 1
         self.sorted_word_count_pair = sorted(self.word_to_count.items(), key=operator.itemgetter(1), reverse=True)
 
 
